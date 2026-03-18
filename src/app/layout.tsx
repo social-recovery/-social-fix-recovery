b8e6827180aa6990e27f25fae0c20dd331e7452e
unknown <you@example.com>
2026-03-18 10:58:54 +0500

    Add Google verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="-G_udtcYZ9Gx5GQkexPh_DGn93DCqT781hiu47BlkYA" />
+      </head>
       <body className={`${geistSans.variable} antialiased`}>
         {children}
       </body>
